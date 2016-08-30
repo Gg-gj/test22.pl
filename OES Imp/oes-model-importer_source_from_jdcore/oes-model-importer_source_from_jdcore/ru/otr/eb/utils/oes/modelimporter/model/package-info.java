e1cfcf766884d2e1c2d031f462aaca21ed63fb88
Gg-gj <gleb10228@gmail.com>
2016-08-30 17:20:56 +0300

    Удаленно лишнее

--- OES Imp/oes-model-importer_source_from_jdcore/oes-model-importer_source_from_jdcore/ru/otr/eb/utils/oes/modelimporter/model/package-info.java
+++
@@ -1,6 +0,0 @@
-package ru.otr.eb.utils.oes.modelimporter.model;
-
-import javax.xml.bind.annotation.XmlSchema;
-
-@XmlSchema(namespace="http://www.otr.ru/eb/policystore")
-abstract interface package-info {}
